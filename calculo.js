dc00d86c9830fc89db75b23bd76445d8d3410f2b
LIVINHALINDA <133163526+LIVINHALINDA@users.noreply.github.com>
2023-11-24 16:37:19 -0300

    Add files via upload
    primeira versão sem estilização.

---
+++ calculo.js
@@ -0,0 +1,28 @@
+function calcularRaizes() {
+    // Obtém os coeficientes da equação quadrática do HTML
+    const a = parseFloat(document.getElementById('coeficienteA').value);
+    const b = parseFloat(document.getElementById('coeficienteB').value);
+    const c = parseFloat(document.getElementById('coeficienteC').value);
+
+    // Verifica se os coeficientes são números
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        document.getElementById('resultado').innerText = 'Por favor, insira coeficientes válidos.';
+        return;
+    }
+
+    // Calcula o discriminante
+    const delta = b * b - 4 * a * c;
+
+    // Verifica se as raízes são reais
+    if (delta < 0) {
+        document.getElementById('resultado').innerText = 'As raízes são números complexos.';
+        return;
+    }
+
+    // Calcula as raízes
+    const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+    // Exibe o resultado no HTML
+    document.getElementById('resultado').innerText = `As raízes são: x1 = ${x1.toFixed(2)}, x2 = ${x2.toFixed(2)}`;
+}
